d462e00846603f875fa38c7254991f76480d161f
fine <92whdtjs@gmail.com>
2025-03-23 21:33:05 +0900

    Xcode Project Setting

---
+++ SimpleSandSimul/SimpleSandSimul/SimpleSandSimulApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSandSimulApp.swift
+//  SimpleSandSimul
+//
+//  Created by 이종선 on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSandSimulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
